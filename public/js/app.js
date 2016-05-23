c3fef8034a32b551e1594f76a8c7c186c50ea9e3
Mauro Marano <gagginaspinnata@MBP-di-Mauro.homenet.telecomitalia.it>
2016-05-23 18:08:35 +0200

    aggiunto un contatore che conta quante volte sono stati effettuati i calcoli

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-// function addOperation(num) {
-//     firebase.database().ref('operations/').set({
-//         operations: num + 1
-//     });
-// }
-
-// function getOperations(cb) {
-//     firebase.database().ref('operations/').on('value', function(snapshot) {
-//         cb(snapshot.val())
-//     });
-// }
-
 $(document).ready(function() {
     $('#submit').click(function(e) {
         e.preventDefault();
@@ -23,9 +11,18 @@ $(document).ready(function() {
             var ris_prodotto = (quantita_soluzione / parti_totali);
             var ris_acqua = ris_prodotto * parti_acqua;
             $('#result').html("<div class=\"panel panel-success\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Risultato</h3></div><div class=\"panel-body\">Per ottenre una soluzione di " + quantita_soluzione + " ml dovrai utilizzare <strong>" + ris_acqua.toFixed(2) + "</strong> ml di acqua e <strong>" + ris_prodotto.toFixed(2) + "</strong> ml di prodotto</div></div>");
-            // getOperations(function(){
-                
-            // });
+
+
+            firebase.database().ref('operations/').once('value').then(function(snapshot) {
+                var op = snapshot.val()+1;
+                firebase.database().ref('/').set({
+                    operations: op
+                });
+            });
         }
     });
+
+    // firebase.database().ref('operations/').on('value',function(snapshot){
+    //     $('#number_of_operations').html(snapshot.val());
+    // });
 });
