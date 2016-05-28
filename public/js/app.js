229dbdff74f9b548a9b1814878e4eda76b37f234
Mauro <gagginaspinnata@gmail.com>
2016-05-28 12:58:44 +0200

    aggiunto un messaggio di errore quando viene premuto il pulsante calcola senza aver specificato la quantita di soluzione desiderata. Aggiunto un font personalizzato ed un po di css

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
+
+    var alertDiv = $('#alert');
+
+    function showAlert(type,message){
+        alertDiv.append("<div class=\"alert alert-"+type+" alert-dismissible\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button><strong> "+message+"</strong></div>")
+    }
+
+    function deleteAlert(){
+        alertDiv.html("");
+    }
+
+    // Quando viene premuto il pulsante calcola
     $('#submit').click(function(e) {
         e.preventDefault();
         var parti_acqua = parseInt($('#parti-acqua').val());
         var parti_prodotto = parseInt($('#parti-prodotto').val());
         var quantita_soluzione = parseInt($('#quantita-soluzione').val());
+
+        // Se il vaore quantità soluzione è stato impostato
         if (quantita_soluzione) {
+
+            // Elimina eventuali messaggi di errore mostrati precedentemente
+            deleteAlert();
 
             var parti_totali = parti_acqua + parti_prodotto;
 
@@ -19,6 +36,11 @@ $(document).ready(function() {
                     operations: op
                 });
             });
+        }
+
+        // Se non è stato impostato il valore quantià soluzione mostra un errore
+        else{
+            showAlert('danger','Inserisci la quantita\' di soluzione desiderata!');
         }
     });
 
